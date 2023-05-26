c2c1329b71e6f9bfbb2e521a741d7b16f8d53f9d
Aakash Mukherjee <110044436+blackcode1996@users.noreply.github.com>
2023-05-26 23:07:31 +0530

    integration

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://chatapp-orcin-two.vercel.app',
     methods: ['GET', 'POST']
   }
 })
